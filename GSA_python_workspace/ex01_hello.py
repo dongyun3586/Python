1b103cf052ca88028cdce533b13eb04a74b09752
leedong <goodluck3586@gmail.com>
2022-01-16 23:52:50 +0900

    gg

--- GSA_python_workspace/ex01_hello.py
+++ GSA_python_workspace/ex01_hello.py
@@ -1,3 +1,12 @@
-print("Hello world!")
+import random
 
-print()
+def generateRandomNum(n):
+    arr = []
+    for i in range(n):
+        arr.append(random.random())
+        
+
+n = 20
+generateRandomNum(n)
+arr = generateRandomNum(n)
+arr
