7042f1abc97103f856e828f92c05cb303b784188
leedong <goodluck3586@gmail.com>
2022-03-05 14:18:49 +0900

    gg
    gg

--- Automation/imageSearchClick.py
+++ Automation/imageSearchClick.py
@@ -1,8 +1,8 @@
 # 화면을 감시하다가 다음으로 넘어가라는 이미지가 발견되면 다음 버튼을 클릭한다.
-
 import pyautogui as pag
 import time
 import keyboard
+import os
 
 # 1. 다음으로 넘어가는 버튼의 좌표 저장
 print('다음으로 넘어가는 버튼의 위치에서 F4키를 누르시오')
@@ -15,15 +15,24 @@ while True:
 time.sleep(1)
 
 # 2. 화면 학습이 종료되었다는 메시지가 나타나면 다음 버튼 클릭
+# 화면 종료 이미지 파일 이름 리스트 만들기
+path_imgDir = '.\images'    # 현재 폴더의 하위 폴더 'images'
+imgFile_list = os.listdir(path_imgDir)
+print(imgFile_list)
+
 while True:
     
     # 학습 종료 이미지 검색
-    print('학습 종료 이미지 검색중') 
+    print('스크린에서 현재 페이지 학습완료 이미지 검색중') 
     while True:
-        for i in range(1, 4):
-            nextImgList = list(pag.locateAllOnScreen('./images/next{}.png'.format(i), confidence=0.95))
+        for imgFile in imgFile_list:
+            nextImgList = list(pag.locateAllOnScreen('./images/' + imgFile, confidence=0.95))
             if nextImgList:
                 break
+        # for i in range(1, 6):
+        #     nextImgList = list(pag.locateAllOnScreen('./images/next{}.png'.format(i), confidence=0.95))
+        #     if nextImgList:
+        #         break
         # 빈 리스트가 아니라면 True
         if nextImgList:
             nextImg_pos = nextImgList[0]
