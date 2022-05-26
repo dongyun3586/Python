300cdd5296557ebbd51f030f5752c6a956607ff6
leedong <goodluck3586@gmail.com>
2022-05-27 08:15:07 +0900

    gg
    gg

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-def func(a, b, c, d=0, e=1, *f, g, h=3, **i):
-  print(a,b,c,d,e, f, g, h, i)
-  
+import random
 
-func(1,2,3,4,5,6,7,8, g=10, x='a', y='b', z='c')
-# func(1,2,3)
+l = [i for i in range(10)]
+print(l)
+
+print(random.choice(l))
+
+random.shuffle(l)
+
+print(l)
